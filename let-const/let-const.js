4b6a53a18af32f2b36415eade1db654ebda153fa
TomatoGit <chenfanlmm@163.comssh-keygen -t rsa -C chenfanlmm@163.com>
2019-08-20 08:48:50 +0800

    作用域的探索

--- let-const/let-const.js
+++ let-const/let-const.js
@@ -29,8 +29,53 @@
 //函数的声明部分会变量提升
 
 //注：如果是这样
-foo();  //TypeError: foo is not a function  变量提升了，只是变量类型不是函数，调用错误
-var foo=function(){
-    console.log("hello");
-}
+// foo();  //TypeError: foo is not a function  变量提升了，只是变量类型不是函数，调用错误
+// var foo=function(){
+//     console.log("hello");
+// }
 //等号右边的function定义的函数不会变量提升
+
+
+// let a=10;
+// let a=20;   //SyntaxError: Identifier 'a' has already been declared     //ES6规定，let声明的变量，不可以重复声明
+// console.log(a);
+
+//ES6只有全局作用域和函数作用域，没有块级作用域
+// var a=new Date();
+// function f(){
+//     console.log(a);
+//     if(false){
+//         var a="hello false";
+//     }
+// }
+// f();    //undefined
+//分析：原意是想输出函数外部Date的值，但是if块中的声明属于函数作用域，变量提升，覆盖了外部变量的声明
+
+//探索
+// let a=0;
+// {
+//     let a=2;
+//     console.log(a); //2
+// }
+// console.log(a); //0
+//结论：大括号内外的let声明不会发生重复声明覆盖和冲突
+
+//new探索
+// let a=0;
+// if(true){
+//     // let a=2;
+//     console.log(a); //2
+// }
+//if的外部作用域和内部作用域是父子关系，和for循环有些类似
+
+//探索new1
+// let a=0;
+// {
+//     console.log(a); //0     外部作用域和大括号内部作用域是父作用域和自作用域的关系
+// }
+
+//探索
+{
+    let a=0;
+}
+console.log(a); //ReferenceError: a is not defined  大括号内部的声明不能用于父作用域
